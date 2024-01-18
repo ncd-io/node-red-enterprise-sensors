414f2c64467c7bfa2c27c282a32dd491894a6787
telliottosceola <travis@controlanything.com>
2024-01-18 09:38:22 -0600

    fixed repo

--- node-red-enterprise-sensors/index.js
+++
@@ -1,9 +0,0 @@
-process.on('unhandledRejection', (reason, p) => {
-  console.log({'Unhandled Rejection at': p, reason: reason});
-  // application specific logging, throwing an error, or other logic here
-});
-
-module.exports = {
-	Modem: require("./lib/DigiParser.js"),
-	Gateway: require("./lib/WirelessGateway.js")
-}
