1c4969c59e355f1c260fab4a07446c8fc8edd5a1
io-Jacob <jcy.youngblood@gmail.com>
2024-01-25 14:27:19 -0600

    Added t107 added t39 t52

--- wireless.js
+++ wireless.js
@@ -857,6 +857,20 @@ module.exports = function(RED) {
 								// }
 								// promises.set_rtc_101 = node.config_gateway.config_set_rtc_101(mac);
 								break;
+							case 107:
+								if(config.sensor_boot_time_420ma_active){
+									promises.sensor_boot_time_420ma = node.config_gateway.config_set_sensor_boot_time_420ma(mac, parseInt(config.sensor_boot_time_420ma));
+								}
+								if(config.low_calibration_420ma_active){
+									promises.low_calibration_420ma = node.config_gateway.config_set_low_calibration_420ma(mac, parseInt(config.low_calibration_420ma));
+								}
+								if(config.mid_calibration_420ma_active){
+									promises.mid_calibration_420ma = node.config_gateway.config_set_mid_calibration_420ma(mac, parseInt(config.mid_calibration_420ma));
+								}
+								if(config.high_calibration_420ma_active){
+									promises.high_calibration_420ma = node.config_gateway.config_set_high_calibration_420ma(mac, parseInt(config.high_calibration_420ma));
+								}
+								break;
 							case 200:
 								if(config.low_calibration_420ma_active){
 									promises.low_calibration_420ma = node.config_gateway.config_set_low_calibration_420ma(mac, parseInt(config.low_calibration_420ma));
