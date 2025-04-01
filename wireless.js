c097bf8cbff00ac10392d7084720c1770a6da6cd
io-Jacob <jcy.youngblood@gmail.com>
2025-04-01 10:39:31 -0500

    v1.3.2 t108 changes to main
    Added fly interval cmd t108
    Added sample rate cmd t108
    Added tooltip fw version in sample rate cmd t108
    Fixed typo in sample rate cmd t108
    Fixed cmds t108

--- wireless.js
+++ wireless.js
@@ -1959,6 +1959,12 @@ module.exports = function(RED) {
 								if(config.quality_of_service_108_active){
 									promises.quality_of_service_108 = node.config_gateway.config_set_quality_of_service_108(mac, parseInt(config.quality_of_service_108));
 								}
+								if(config.fly_interval_108_active){
+ 									promises.fly_interval_108 = node.config_gateway.config_set_fly_interval_108(mac, parseInt(config.fly_interval_108));
+ 								}
+								if(config.sample_rate_108_active){
+ 									promises.sample_rate_108 = node.config_gateway.config_set_sample_rate_108(mac, parseInt(config.sample_rate_108));
+ 								}
 								break;
 							case 110:
 								if(config.odr_p1_110_active){
